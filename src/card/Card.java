a30d78909780f1ef11cd9add2db43681a398b5bd
Abhishek Abhishek <abhish31@sheridancollege.ca>
2025-05-23 23:31:47 -0400

    Update Card.java

--- src/card/Card.java
+++ src/card/Card.java
@@ -11,7 +11,6 @@ package card;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author Abhishek
  * @modifier Abhishek Abhishek
  * Student ID: 991760197
  * Date Modified: May 23, 2025
